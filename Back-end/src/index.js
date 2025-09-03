61b0ad25408005cd6f072169293494153e93500e
Kulith_Renuja.17 <krenuja85@gmail.com>
2025-09-04 03:41:00 +0530

    feat: add User and Booking schemas, controllers, and routes

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import express from "express";
-import hotelsRouter from "./api/hotel.js";
 import connectDB from "./infrastructure/db.js";
+
+import hotelsRouter from "./api/hotel.js";
+import usersRouter from "./api/user.js";
+import bookingsRouter from "./api/booking.js";
 
 // Create an Express instance
 const app = express();
@@ -12,6 +15,8 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/hotels", hotelsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/bookings", bookingsRouter);
 
 
 // Define the port to run the server
